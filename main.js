f1eb8c6f68b936a3edb32de969b353ec242b21b9
jzy <ZiyiKing@outlook.com>
2014-12-19 11:43:48 +0800

    fixed on javascript files and complite fillin text

--- main.js
+++ main.js
@@ -2,27 +2,25 @@ function checkform(){
 var score = 0;
 
   var fillIn1 = new Fillin("blank1_1","统一建模语言");
-  //alert('rr');
   score += fillIn1.getscore();
-  // var fillIn2 = new fill_in("blank2_1","封装性");
-  // score += fillIn2.getscore();
-  // var fillIn3 = new fill_in("blank2_2","继承性");
-  // score += fillIn3.getscore();
-  // var fillIn4 = new fill_in("blank2_3","多态性");
-  // score += fillIn4.getscore();
-  // var fillIn5 = new fill_in("selection1","B");
-  // score += fillIn5.getscore();
-  // var fillIn6 = new fill_in("selection2","A");
-  // score += fillIn6.getscore();
-  // var ChoiceAnswer1 = new ChoiceAnswer("choice3","ABD");
-  // score += ChoiceAnswer1.getscore();
-  // var ChoiceAnswer2 = new ChoiceAnswer("choice4","ABC");
-  // score += ChoiceAnswer2.getscore();
-  // var ChoiceAnswer3 = new ChoiceAnswer("judge","NO");
-  // score += ChoiceAnswer3.getscore();
-  // var ChoiceAnswer4 = new ChoiceAnswer("judge","YES");
-  // score += ChoiceAnswer4.gChoiceAnswer4etscore();
-  // var fillIn11 = new fill_in("judge","YES");
+  var fillIn2 = new Fillin("blank2_1","封装性");
+  score += fillIn2.getscore();
+  var fillIn3 = new Fillin("blank2_2","继承性");
+  score += fillIn3.getscore();
+  var fillIn4 = new Fillin("blank2_3","多态性");
+  score += fillIn4.getscore();
+  var fillIn5 = new Fillin("selection1","B");
+  score += fillIn5.getscore();
+  var fillIn6 = new Fillin("selection2","A");
+  score += fillIn6.getscore();
+  var ChoiceAnswer1 = new ChoiceAnswer("choice3","ABD");
+  score += ChoiceAnswer1.getscore();
+  var ChoiceAnswer2 = new ChoiceAnswer("choice4","ABC");
+  score += ChoiceAnswer2.getscore();
+  var ChoiceAnswer3 = new ChoiceAnswer("judge","NO");
+  score += ChoiceAnswer3.getscore();
+  var ChoiceAnswer4 = new ChoiceAnswer("judge","YES");
+  score += ChoiceAnswer4.getscore();
   // score += fillIn1.getscore("answer","模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。");
   //
 
